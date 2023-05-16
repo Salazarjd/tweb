2184851c0d7bf0d3ebe902343f41029dce3dc05c
Juan Diego Salazar <salazar.jd@gmail.com>
2023-05-15 22:55:22 -0500

    eliminando paquete proyecto

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const tipoProyecto = require('./routes/tipoProyecto')
 const cliente = require('./routes/cliente')
 const etapa = require('./routes/etapa')
 const universidad = require('./routes/unversidad')
-const proyecto = require('./routes/proyecto')
 
 
 // middlewares
@@ -21,7 +20,6 @@ app.use('/api/tiposproyectos', tipoProyecto);
 app.use('/api/clientes', cliente);
 app.use('/api/etapas', etapa);
 app.use('/api/universidades', universidad);
-app.use('/api/proyectos', proyecto);
 
 
 module.exports = app
